5f3f19688e42ab0e1fd5747ea3da98d5e7c83f8c
abdlrohmn <abdlrohmn@gmail.com>
2026-04-01 22:48:55 +0700

    intial commit

--- public/js/script.js
+++ public/js/script.js
@@ -5,9 +5,9 @@
 // 1. DATABASE SEMENTARA (Simulasi Array Data Produk)
 // Nanti di UAS, data ini akan diambil dari MySQL via CodeIgniter.
 const dataProduk = [
-    { id: 1, nama: "Paket Website Basic", harga: 1500000, icon: "fa-laptop-code" },
-    { id: 2, nama: "Jasa SEO Audit", harga: 800000, icon: "fa-magnifying-glass-chart" },
-    { id: 3, nama: "Manajemen Sosmed", harga: 2500000, icon: "fa-hashtag" }
+    { id: 1, nama: "Sekedah Sampah", harga: 1500000, icon: "fa-laptop-code" },
+    { id: 2, nama: "Kepul Point", harga: 6000000, icon: "fa-magnifying-glass-chart" },
+    { id: 3, nama: "Sekedah Sampah", harga: 2500000, icon: "fa-hashtag" }
 ];
 
 // STATE APLIKASI (Variabel untuk melacak status transaksi)
@@ -88,7 +88,7 @@ function cekPromoOtomatis() {
     // Jika totalKeranjang LEBIH DARI Rp 2.000.000, berikan pesan diskon.
     // Jika tidak, hilangkan pesan diskon/beri pesan upselling.
 
-    if (totalKeranjang > 2000000) {
+    if (totalKeranjang > 5000000) {
         // Tampilkan peringatan promo
         promoAlert.classList.remove('d-none');
         promoAlert.classList.replace('alert-info', 'alert-success');
@@ -97,7 +97,7 @@ function cekPromoOtomatis() {
         // Sembunyikan peringatan jika total turun (opsional untuk keranjang dinamis)
         // Untuk saat ini, kita beri dorongan upselling
         promoAlert.classList.remove('d-none');
-        teksPromo.textContent = `Tambah Rp ${(2000000 - totalKeranjang).toLocaleString('id-ID')} lagi untuk dapat Diskon 10%!`;
+        teksPromo.textContent = `Tambah Rp ${(5000000 - totalKeranjang).toLocaleString('id-ID')} lagi untuk dapat Diskon 10%!`;
     }
 }
 
